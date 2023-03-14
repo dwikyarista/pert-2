a37c4bdd4e2494ca53c61af0c087d5266324fd7a
emaildwikyarista@students.amikom.ac.id <arif.kurniawan@students.amikom.ac.id>
2023-03-14 14:58:40 +0700

    commit ke-1

---
+++ Aplikasi Kalkulator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikasi_Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ini apps sederhana menggunakan ";
+            Console.Write("Operasi apa yang ingin diterjemahkan? \n 1, '+'\n 2, '-' \n 3.
+        }
+    }
+}
